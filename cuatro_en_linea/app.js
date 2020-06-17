e2deb55f147611e27b11b3bc1672112f33881958
CesarPagliarini <capagliarini@gmail.com>
2020-06-17 13:15:08 -0300

    Finalizacion por linea oblicua - agregada

--- cuatro_en_linea/app.js
+++ cuatro_en_linea/app.js
@@ -34,8 +34,8 @@ var pintarFicha = function() {
 }
 
 var finDelJuegoHorizontal = function() {                          // Si 4 lugares contiguos en forma horizontal estan ocupados por el mismo color, termina el juego.
-    for( let y = 0; y < tablero.length; y+=1){
-       for(let x =0; x<4;x+=1){
+    for( let y=0; y < tablero.length; y+=1){
+       for(let x=0; x<4; x+=1){
            if(tablero[y][x] === tablero[y][x+1] && tablero[y][x+1] === tablero[y][x+2] && tablero[y][x+2] === tablero[y][x+3] && tablero[y][x] !=0){
                alert("Fin del juego")
            }
@@ -44,8 +44,8 @@ var finDelJuegoHorizontal = function() {                          // Si 4 lugare
 }
 
 var finDelJuegoVertical = function() {                          // Si 4 lugares contiguos en forma vertical estan ocupados por el mismo color, termina el juego.
-    for( let x = 0; x < tablero.length; x+=1){
-       for(let y =0; y<3;y+=1){
+    for( let x=0; x < tablero.length; x+=1){
+       for(let y=0; y<3; y+=1){
            if(tablero[y][x] === tablero[y+1][x] && tablero[y+1][x] === tablero[y+2][x] && tablero[y+2][x] === tablero[y+3][x] && tablero[y][x] !=0){
                alert("Fin del juego")
            }
@@ -53,7 +53,25 @@ var finDelJuegoVertical = function() {                          // Si 4 lugares 
     }
 }
 
+var finDelJuegoOblicuoDescendente = function() {                 // Si 4 lugares contiguos en forma oblicua (direccion al 4to cuadrante) estan ocupados por el mismo color
+        for( let y=0; y<3; y+=1){                                // finaliza el juego
+            for(let x=0; x<4; x+=1){
+                if(tablero[y][x] === tablero[y+1][x+1] && tablero[y+1][x+1] === tablero[y+2][x+2] && tablero[y+2][x+2] === tablero[y+3][x+3] && tablero[y][x] !=0){
+                    alert("Fin del juego")
+                }
+            }
+        }
+}
 
+var finDelJuegoOblicuoAscendente = function() {                 // Si 4 lugares contiguos en forma oblicua (direccion al 1er cuadrante) estan ocupados por el mismo color
+    for( let y=5; y>2; y-=1){                                   // finaliza el juego
+        for(let x=0; x<4; x+=1){
+            if(tablero[y][x] === tablero[y-1][x+1] && tablero[y-1][x+1] === tablero[y-2][x+2] && tablero[y-2][x+2] === tablero[y-3][x+3] && tablero[y][x] !=0){
+                alert("Fin del juego")
+            }
+        }
+    }
+}
 
 // Codigo principal
 // ################
@@ -70,7 +88,9 @@ for( let y = 0; y < tablero.length; y+=1){
             document.getElementById("Turno").className = "jugador" + turno
             pintarFicha()
             finDelJuegoHorizontal()
-            finDelJuegoVertical()            
+            finDelJuegoVertical()
+            finDelJuegoOblicuoDescendente()            
+            finDelJuegoOblicuoAscendente()
         }
     }
 }
